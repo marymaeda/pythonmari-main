e2957ac5017537378aa4002ed610aced26af4459
MARI <mariana.mendes.maeda@escola.pr.gov.br>
2025-09-24 10:52:32 -0300

    m

---
+++ cap5/exerc1.py
@@ -0,0 +1,7 @@
+def areatriangulo(base, altura):
+    print(f'A area do trianguloe é: {(base * altura) / 2}')
+
+
+baseTriangulo = int(input("Insira a base do triângulo: "))
+alturaTriangulo = int(input("Insira a altura do triângulo: "))
+areatriangulo(baseTriangulo, alturaTriangulo)
