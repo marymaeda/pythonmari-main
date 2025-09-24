a0da1ca5594c9c3e6c28129bb88d75afce4abccf
MARI <mariana.mendes.maeda@escola.pr.gov.br>
2025-09-24 10:36:16 -0300

    exerc1

---
+++ pythonMari/funções de leitura/excercicio1.py
@@ -0,0 +1,5 @@
+def soma(numero,numero1):
+    print(f'A soma dos números é: {numero + numero1}')
+numero = int(input("Informe um número inteiro: "))
+numero1 = int(input("Informe outro número inteiro: "))
+soma(numero,numero1)
